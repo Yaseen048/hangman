cdc09fa5a812fccdbe811f95ae3656128c848401
Yaseen <yas484@hotmail.com>
2023-11-08 01:25:48 +0000

    intialising attributes of hangman class

---
+++ hangman/milestone4.py
@@ -0,0 +1,15 @@
+import random
+
+class Hangman:
+
+    def __init__(self, word_list, num_lives=5):
+
+        self.word_list = word_list
+        self.word = random.choice(word_list)
+        self.num_lives = num_lives
+        self.num_letters = len(set(self.word))
+        self.word_guessed = ['_'] * len(self.word)
+        self.list_of_guesses = []
+
+
+        pass
