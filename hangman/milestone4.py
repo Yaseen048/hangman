cb116ef60814ffbc3ebca33f849414bbe8ddbe41
Yaseen <yas484@hotmail.com>
2023-11-11 18:21:47 +0000

    creates ask for input and check guess methods

--- hangman/milestone4.py
+++ hangman/milestone4.py
@@ -11,6 +11,25 @@ class Hangman:
         self.word_guessed = ['_'] * len(self.word)
         self.list_of_guesses = []
 
+    def check_guess(self, guess):
+        lower_case_guess = guess.lower()
+        if lower_case_guess in word:
+            print(f"Good guess! {guess} is in the word.")
+
+        pass
+
+    def ask_for_input(self):
+        while True:
+            guess = input("pick a letter \n")
+            if guess.isalpha() == False and len(guess) != 1:
+                print("Invalid letter. Please, enter a single alphabetical character.")
+            elif guess in self.list_of_guesses:
+                print("You already tried that letter!")
+            else:
+                check_guess(guess)
+                self.list_of_guesses.append(guess)
+            print("Invalid letter. Please, enter a single alphabetical character.")
+
 
 # Printing attributes
 '''
