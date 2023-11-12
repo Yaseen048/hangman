d461a158bc1188cc8c6c1168046ceffda5134ac3
Yaseen <yas484@hotmail.com>
2023-11-12 02:39:56 +0000

    comments on code

--- hangman/milestone4.py
+++ hangman/milestone4.py
@@ -3,7 +3,7 @@ import random
 class Hangman:
 
     def __init__(self, word_list, num_lives=5):
-
+        
         self.word_list = word_list
         self.word = random.choice(word_list)
         self.num_lives = num_lives
@@ -13,15 +13,16 @@ class Hangman:
 
 
     def check_guess(self, guess):
-        lower_case_guess = guess.lower()
+        lower_case_guess = guess.lower()#turning guess into lowercase
         if lower_case_guess in self.word:
             print(f"Good guess! {lower_case_guess} is in the word.")
             for letter in self.word:
+                #replacing undercore with correct letter
                 if guess == letter:
                     letter_index = self.word.index(letter)
                     self.word_guessed[letter_index] = guess
             self.num_letters -= 1
-        else:
+        else:#if incorrect letter guess, number of lives reduces by 1
             self.num_lives -= 1
             print(f"Sorry, {lower_case_guess} is not in the word.")
             print(f"You have {self.num_lives} lives left.")
@@ -31,12 +32,12 @@ class Hangman:
     def ask_for_input(self):
         while True:
             guess = input("pick a letter \n")
-            if guess.isalpha() == False or len(guess) != 1:
+            if guess.isalpha() == False or len(guess) != 1:#checks for valid input
                 print("Invalid letter. Please, enter a single alphabetical character.")
-            elif guess in self.list_of_guesses:
+            elif guess in self.list_of_guesses:#if user has repeated guess
                 print("You already tried that letter!")
             else:
-                self.check_guess(guess)
+                self.check_guess(guess)#for new guess
                 self.list_of_guesses.append(guess)
                 print(self.list_of_guesses)
             
